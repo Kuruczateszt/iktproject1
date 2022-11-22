a57697699e78516445cf6e9c9e8dddd1f2b6419b
Kurucz Attila <kurucza@kkszki.hu>
2022-11-22 17:42:43 +0100

    gondoltam egy számra

--- projectek/Python_jo_napod_van/feladat.py
+++ projectek/Python_jo_napod_van/feladat.py
@@ -1,3 +1,5 @@
+import random
+
 def jo_napod_van_e():
     ered = ""
     while ered not in ["igen", "nem"]:
@@ -17,11 +19,34 @@ def paros_e():
         except ValueError:
             print("csak számokat adhatsz meg: ")
 
-    marad = ered % 2
-    if marad != 0:
+    if ered % 2 != 0:
         print("A beírt szám páratlan")
     else:
         print("A beírt szám páros")
 
+def gondoltam_egy_szamra():
+    gondol = random.randrange(1, 6)
+    ered = None 
+    while ered is None:
+        szam = None
+        try:
+            szam = int(input("Adj meg egy számot 1 és 5 között: "))
+        except ValueError:
+            print("csak számokat adhatsz meg: ")
+            continue
+        if szam not in [1, 2, 3, 4, 5]:
+            print("Nem megfelelő számot adtál meg")
+        else:
+            ered = szam
+
+    print(f"Az általad megadott szám az {ered}")
+    if ered == gondol:
+        print("Kitaláltad a gép által gondolt számot")
+    elif ered < gondol:
+        print("A gép nagyobb számra gondolt")
+    else:
+        print("A gép kisebb számra gondolt")
+
 # jo_napod_van_e()
-paros_e()
+# paros_e()
+gondoltam_egy_szamra()
